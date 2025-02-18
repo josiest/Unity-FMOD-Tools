2f82b09e037298d70e2381533901def50b4df2f8
Josie Thompson <dot.slash.josie@gmail.com>
2025-02-18 12:33:43 -0800

    added FMOD audio tools from Dorsal Fin project added fmod plugin

---
+++ Runtime/AudioBus.cs
@@ -0,0 +1,65 @@
+﻿using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+namespace Pi.FMODTools
+{
+    [UnityEngine.CreateAssetMenu(menuName="Pi Toolset/FMOD Tools/Audio Bus")]
+    public class AudioBus : ScriptableObject
+    {
+        // Configuration
+        [Tooltip("The FMOD path for the bus - empty for master bus")]
+        public string Path;
+        
+        [Range(0f, 1f)]
+        [Tooltip("Value for volume on a scale of 0-1")]
+        [SerializeField] private float volume = 1f;
+
+        // Public Interface
+        public delegate void VolumeChangedEvent(float value);
+        public VolumeChangedEvent OnVolumeChanged;
+
+        public void Initialize()
+        {
+            if (RuntimeManager.IsInitialized)
+            {
+                bus = RuntimeManager.GetBus($"bus:/{Path}");
+                bus.setVolume(volume);
+            }
+        }
+
+        public float Volume
+        {
+            get
+            {
+                if (!bus.isValid()) { Initialize(); }
+                return volume;
+            }
+            set
+            {
+                if (Mathf.Approximately(volume, value)) { return; }
+                volume = value;
+                UpdateVolume();
+            }
+        }
+        
+        // Internal Interface
+        private Bus bus;
+
+        private void UpdateVolume()
+        {
+            if (RuntimeManager.IsInitialized)
+            {
+                if (bus.isValid()) { bus.setVolume(volume); }
+                OnVolumeChanged?.Invoke(volume);
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            UpdateVolume();
+        }
+#endif
+    }
+}
---
+++ Runtime/SimpleMusicPlayer.cs
@@ -0,0 +1,35 @@
+﻿using FMOD.Studio;
+using FMODUnity;
+using UntyEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
+
+namespace Pi.FMODTools {
+    public class SimpleMusicPlayer : UnityEngine.MonoBehaviour
+    {
+        // Configuration
+        [Header("General")]
+        [Tooltip("The music to play when this objet is loaded")]
+        public EventReference Music;
+
+        [Header("Advanced")]
+        [Tooltip("Should the music cut without fading when this object is destroyed (not recommended)?")]
+        public bool NoFadeOut;
+        
+        // Internal interface
+        private EventInstance musicInstance;
+        
+        // Unity Events
+        private void Start()
+        {
+            musicInstance = RuntimeManager.CreateInstance(Music);
+            if (musicInstance.isValid()) { musicInstance.start(); }
+        }
+
+        private void OnDestroy()
+        {
+            if (!musicInstance.isValid()) { return; }
+            musicInstance.stop(NoFadeOut? STOP_MODE.IMMEDIATE : STOP_MODE.ALLOWFADEOUT);
+            musicInstance.release();
+        }
+    }
+}
